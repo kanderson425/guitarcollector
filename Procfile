e07d68e46ae21689f7835e2f8057eac47940dd66
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro-2.local>
2019-04-01 14:35:25 -0500

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn guitar-collector-app.wsgi
--- guitarcollector/settings.py
+++ guitarcollector/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/guitars/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
